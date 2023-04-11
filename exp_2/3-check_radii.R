545112617bdd6618f06e7aa0a5c2840855468531
Megan Chiovaro <megan.chiovaro@uconn.edu>
2023-04-11 11:06:01 -0400

    Add radii checks and MdRQA calculations for exp 2

---
+++ exp_2/3-check_radii.R
@@ -0,0 +1,40 @@
+##### Check radii #####
+#
+# Check how the radius search went and see if we need to expand anywhere.
+# 
+# Code by: @mchiovaro
+# Last updated: 2023_02_15
+
+#### 1. set up ####
+rm(list=ls())
+setwd("./Documents/github/division-of-labor-analyses/exp_2")
+
+# read in data
+data = read.table('./data/data_prepped-exp_2.csv', 
+                  sep=',', header = TRUE)
+
+radius_files_all = list.files('./data/radii_all', 
+                                  pattern='radii*',
+                                  full.names = TRUE)
+
+radius_stats_all = rbind.data.frame(rbindlist(lapply(radius_files_all, fread, sep=","))) %>%
+  ungroup() %>%
+  group_by(chosen.participant) %>%
+  dplyr::filter(from.target==min(from.target)) %>%
+  dplyr::arrange(chosen.participant) %>%
+  dplyr::rename(dyad.round = chosen.participant) %>%
+  filter(row_number()==n())
+
+# identify dyad.rounds that are still too far from target rr and save
+recheck_radii = radius_stats_all %>% ungroup() %>%
+  dplyr::filter(from.target > .5) %>%
+  dplyr::select(dyad.round, chosen.radius, rr, from.target)
+write.table(recheck_radii,'./data/dropped_dyad.rounds.csv', sep=',',row.names=FALSE,col.names=TRUE)
+
+# filter and save the final radii
+radii_final = radius_stats_all %>% ungroup() %>%
+  dplyr::filter(from.target < .5) %>%
+  dplyr::select(dyad.round, chosen.radius, rr, from.target)
+data_with_radii = full_join(x=radii_final,
+                  y=data, by=c("dyad.round"="dyad.round"))
+write.table(data_with_radii,'./data/data_with_radii.csv', sep=',',row.names=FALSE,col.names=TRUE)
---
+++ exp_2/4-mdrqa.R
@@ -0,0 +1,63 @@
+##### 4. Check radii #####
+#
+# Run MdRQA with identified radii.
+# 
+# Code by: @mchiovaro
+# Last updated: 2023_02_25
+
+#### 1. set up ####
+rm(list=ls())
+setwd("./Documents/github/division-of-labor-analyses/exp_2")
+
+# read in data
+data = read.table('./data/data_with_radii.csv', 
+                  sep=',', header = TRUE)
+
+# use the MdRQA file
+source(file = "mdrqa.R")
+
+# slice up the data so that we have one dataset per dyad-round
+split_rounds = split(data,
+                     list(data$dyad.round))
+
+# remove the dyad.rounds that had too high rr
+split_rounds[c(63, 109, 124, 125)] = NULL
+
+# empty frame for results
+mdrqa_results = data.frame()
+
+for (next_round in split_rounds){
+  
+  # isolate parameters for next dyad.round
+  chosen.delay = 1
+  chosen.embed = 1
+  chosen.radius = unique(next_round$chosen.radius)
+  
+  # print update
+  print(paste("CRQA: dyad.round ", unique(next_round$dyad.round)))
+  
+  # run MdRQA
+  rec_analysis = mdrqa(data = as.matrix(next_round$sin, next_round$cos), 
+                       emb = 1, # standard for MdRQA
+                       del = 1, # standard for MdRQA
+                       norm = "euc", 
+                       rad = next_round$chosen.radius)
+  
+  # save to dataframe
+  next_data_line = data.frame(c(unique(next_round$dyad.round),
+                                rec_analysis[1:15]))
+  names(next_data_line) = c("dyad.round",names(rec_analysis[1:15]))
+  mdrqa_results = rbind.data.frame(mdrqa_results,next_data_line)
+  
+}
+
+# grab information about experiment condition
+additional_dyad_info = data %>% ungroup() %>%
+  select(dyad.round,dyad,round_number,td_condition,ie_condition,com_condition,timer) %>% 
+  distinct()
+
+# merge recurrence analyses and condition information
+recurrence_df = plyr::join(mdrqa_results, additional_dyad_info,
+                           by=c('dyad.round'))
+# save to file
+write.table(recurrence_df,'./data/mdrqa_and_conditions.csv',sep=',')
