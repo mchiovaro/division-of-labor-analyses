c57959105d72c2730aa3a52dd82d40d79fa8f949
Megan Chiovaro <megan.chiovaro@uconn.edu>
2023-04-16 15:09:56 -0400

    Fix standardization

--- exp_2/5-LMEM.R
+++ exp_2/5-LMEM.R
@@ -15,30 +15,22 @@ data = read.table('./data/mdrqa_and_conditions.csv',
                        sep=',', header = TRUE)
 
 #### 3. Standardize variables and create factors ####
-data_st = data %>% 
-  mutate(DET = scale(DET)) %>% 
-  mutate(timer = scale(timer)) %>% 
-  mutate(round_number = scale(round_number)) %>%
-  mutate(dyad = as.factor(dyad)) %>%
-  mutate(td_condition = as.factor(td_condition)) %>%
-  mutate(ie_condition = as.factor(ie_condition)) %>%
-  mutate(com_condition = as.factor(com_condition))
 data = data %>%
   mutate(dyad = as.factor(dyad)) %>%
   mutate(td_condition = as.factor(td_condition)) %>%
   mutate(ie_condition = as.factor(ie_condition)) %>%
   mutate(com_condition = as.factor(com_condition))
+data_st = data %>% 
+  mutate(DET = scale(DET)) %>% 
+  mutate(timer = scale(timer))
 
 ###### 2. Construct the model for H1-H3 ######
 
 # raw model
 completion.model.raw = lmer(timer ~ 
               # fixed effects
-              td_condition + ie_condition + 
-              com_condition + round_number +
-              td_condition*ie_condition +
-              td_condition*com_condition +
-              ie_condition*com_condition +
+              td_condition + ie_condition + com_condition + round_number +
+              td_condition*ie_condition + td_condition*com_condition + ie_condition*com_condition +
               td_condition*ie_condition*com_condition +
               # random effects of dyad
               (1 | dyad),
@@ -48,11 +40,8 @@ pander_lme(completion.model.raw,stats.caption = TRUE)
 # standardized model
 completion.model.st = lmer(timer ~ 
                               # fixed effects
-                              td_condition + ie_condition + 
-                              com_condition + round_number +
-                              td_condition*ie_condition +
-                              td_condition*com_condition +
-                              ie_condition*com_condition +
+                              td_condition + ie_condition + com_condition + round_number +
+                              td_condition*ie_condition + td_condition*com_condition + ie_condition*com_condition +
                               td_condition*ie_condition*com_condition +
                               # random effects of dyad
                               (1 | dyad),
